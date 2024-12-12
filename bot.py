1b297d1bb4dcd96d87060fa25287a5d0eec554cf
toma331 <arthom.koval@gmail.com>
2024-12-12 23:15:39 +0300

    simple-bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
