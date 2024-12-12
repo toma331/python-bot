d3666204569ee3a34eb27b30b0aeb2ba657f0688
toma331 <arthom.koval@gmail.com>
2024-12-12 23:21:53 +0300

    start

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Type text and bot reply her ;)')
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
